3122b428c8b1769d58c25c64c96e8e38e8fd5cd1
Fanboy Coder <joel.monteiro.ferreira@gmail.com>
2022-07-25 23:50:56 +0100

    First major update

---
+++ javascript.js
@@ -0,0 +1,94 @@
+let myLibrary = [];
+
+let hobbit = new Book("The Hobbit", "JRR Tolkien", 310, "Read");
+let silmarillion = new Book("The Silmarillion", "JRR Tolkien", 365, "Read");
+let lotr = new Book("The Lord of the Rings", "JRR Tolkien", 1000, "Read");
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+    let title = document.getElementById("ftitle").value;
+    let author = document.getElementById("fauthor").value;
+    let pages = document.getElementById("fpages").value;
+    let read = document.querySelector("input[name=radio]:checked").value;
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book);
+    clear();
+    display();
+}
+
+function removeBookFromLibrary(remove) {
+    myLibrary.splice(remove, 1);
+    clear();
+    display();
+    console.log(myLibrary);
+}
+
+function display() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const container = document.getElementById("library");
+        let tr = document.createElement("tr");
+        tr.setAttribute("class", "new-row");
+        container.appendChild(tr);
+        let td = tr.appendChild(document.createElement("td"));
+        let btn = td.appendChild(document.createElement("button"));
+        btn.innerText = "X";
+        btn.setAttribute("id", [i]);
+        btn.setAttribute("class", "delete-button");
+        btn.addEventListener("click", () => {
+            let remove = btn.id;
+            removeBookFromLibrary(remove);
+        })
+        for (let elem in myLibrary[i]) {
+            let td = tr.appendChild(document.createElement("td"));
+            td.setAttribute("class", [elem]);
+            if([elem] == "author" || [elem] == "title" || [elem] == "pages"){
+                td.innerHTML = myLibrary[i][elem];
+            }
+            else {
+                let btn = td.appendChild(document.createElement("button"));
+                btn.innerText = myLibrary[i][elem];
+                // btn.addEventListener("click", () => {
+                //     if([elem] == "read") {
+                //         [elem] == "unread";
+                //     }
+                //     else {
+                //         [elem] == "read";
+                //     }
+                // })
+            }
+        }  
+    }
+}
+
+function clear() {
+    const rows = document.querySelectorAll(".new-row");
+    rows.forEach(row => {
+        row.remove();
+    })
+}
+
+window.onload = function () {
+    myLibrary.push(hobbit, silmarillion, lotr);
+    display();
+    const btn = document.querySelector("#btn");
+    const form = document.querySelector(".book-form");
+    btn.addEventListener("click", () => {
+        form.style.display = "block";
+    })
+    const submit = document.querySelector("#submit");
+    submit.addEventListener("click", () => {
+        addBookToLibrary();
+        const inputs = document.querySelectorAll("#ftitle, #fauthor, #fpages");
+        inputs.forEach(input => {
+            input.value = "";
+        });
+        document.getElementById("read").checked = false;
+        document.getElementById("not-read").checked = false;
+    })
+}
